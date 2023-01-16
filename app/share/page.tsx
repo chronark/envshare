3f38ebe9817e87ccf96f0548c143d6a8fb4d6cef
Jack <59290502+mic0ishere@users.noreply.github.com>
2023-01-16 21:54:01 +0100

    fix: patch form submit without text (#4)
    * fix: don't allow submitting form with empty text
    
    * fix: actually block form submit
    
    * feat: style disabled button
    
    Co-authored-by: Andreas Thomas <dev@chronark.com>

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -86,6 +86,7 @@ export default function Home() {
           className="max-w-3xl mx-auto"
           onSubmit={(e) => {
             e.preventDefault();
+            if (text.length <= 0) return;
             onSubmit();
           }}
         >
@@ -108,6 +109,7 @@ export default function Home() {
                 id="text"
                 name="text"
                 value={text}
+                minLength={1}
                 onChange={(e) => setText(e.target.value)}
                 rows={Math.max(5, text.split("\n").length)}
                 placeholder="DATABASE_URL=postgres://postgres:postgres@localhost:5432/postgres"
@@ -188,10 +190,12 @@ export default function Home() {
           </div>
           <button
             type="submit"
-            disabled={loading}
-            className={`mt-6 w-full h-12 inline-flex justify-center items-center  transition-all  rounded px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-zinc-800   bg-zinc-200 ring-1 ring-transparent duration-150  hover:text-zinc-100 hover:ring-zinc-600/80  hover:bg-zinc-900/20 ${
-              loading ? "animate-pulse" : ""
-            }`}
+            disabled={loading || text.length <= 0}
+            className={`mt-6 w-full h-12 inline-flex justify-center items-center  transition-all  rounded px-4 py-1.5 md:py-2 text-base font-semibold leading-7    bg-zinc-200 ring-1 ring-transparent duration-150   ${
+              text.length <= 0
+                ? "text-zinc-400 cursor-not-allowed"
+                : "text-zinc-900 hover:text-zinc-100 hover:ring-zinc-600/80  hover:bg-zinc-900/20"
+            } ${loading ? "animate-pulse" : ""}`}
           >
             <span>{loading ? <Cog6ToothIcon className="w-5 h-5 animate-spin" /> : "Share"}</span>
           </button>
