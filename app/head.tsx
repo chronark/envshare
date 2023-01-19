6d8644ed94cc92408fa6be999a664994087fee22
chukwumaokere <chukwumaokere@yahoo.com>
2023-01-19 07:11:20 -0600

    fix(api): change og:image size; fix(website): update og tags

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,10 @@
-export default function Head({title}: {title: string}) {
+export default function Head({title, subtitle}: {title: string, subtitle: string}) {
   // Fallback tagline
   if (!title) title = 'Share Environment Variables Securely';
+  if (!subtitle) subtitle = 'EnvShare';
 
   // I'll have to use a different method to generate the image URL, because window can be undefined on the server
-  const url = new URL(`${window.location.href}api/v1/og?title=${encodeURIComponent(title)}`);
+  const url = new URL(`${window.location.href}api/v1/og?title=${encodeURIComponent(title)}&=subtitle=${encodeURIComponent(subtitle)}`);
 
   const imgURL = `${url.toString()}`;
   return (
@@ -12,6 +13,10 @@ export default function Head({title}: {title: string}) {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={title} />
       <meta property='og:image' content={imgURL} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={title} />
+      <meta property="og:image:width" content="1200"/>
+      <meta property="og:image:height" content="630"/>
     </>
   );
 }
