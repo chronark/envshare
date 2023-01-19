42d6282cf7eee7652f39ccdaaa59bf020d281523
chukwumaokere <chukwumaokere@yahoo.com>
2023-01-19 07:07:22 -0600

    fix(website): change localhost to window.location.href

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,8 @@ export default function Head({title}: {title: string}) {
   // Fallback tagline
   if (!title) title = 'Share Environment Variables Securely';
 
-  // I'll have to use a different method to generate the image URL, because window can be undefined
-  const url = new URL(`http://localhost:3000/api/v1/og?title=${encodeURIComponent(title)}`);
+  // I'll have to use a different method to generate the image URL, because window can be undefined on the server
+  const url = new URL(`${window.location.href}api/v1/og?title=${encodeURIComponent(title)}`);
 
   const imgURL = `${url.toString()}`;
   return (
