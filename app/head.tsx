aed671b4eb673357ca6b696ed5f4f19b336baaa0
Andreas Thomas <dev@chronark.com>
2023-01-19 23:22:36 +0100

    feat: use inter

--- app/head.tsx
+++ app/head.tsx
@@ -1,13 +1,15 @@
-import { headers } from 'next/headers';
+import { headers } from "next/headers";
 
-export default function Head({title, subtitle}: {title: string, subtitle: string}) {
+export default function Head({ title, subtitle }: { title: string; subtitle: string }) {
   // Fallback tagline
-  if (!title) title = 'Share Environment Variables Securely';
-  if (!subtitle) subtitle = 'EnvShare';
+  if (!title) title = "Share Environment Variables Securely";
+  if (!subtitle) subtitle = "EnvShare";
 
   const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
-  const url = new URL(`${baseUrl}/api/v1/og?title=${encodeURIComponent(title)}&=subtitle=${encodeURIComponent(subtitle)}`);
+  const url = new URL(
+    `${baseUrl}/api/v1/og?title=${encodeURIComponent(title)}&=subtitle=${encodeURIComponent(subtitle)}`,
+  );
 
   const imgURL = `${url.toString()}`;
   return (
@@ -18,8 +20,8 @@ export default function Head({title, subtitle}: {title: string, subtitle: string
       <meta property='og:image' content={imgURL} />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={title} />
-      <meta property="og:image:width" content="1200"/>
-      <meta property="og:image:height" content="630"/>
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </>
   );
 }
