7f5c6b2b941f5829cd15303ca9ce75ed7f778c60
Chukwuma Okere <chukwumaokere@Yahoo.com>
2023-01-20 03:40:45 -0600

    feat(seo): add remaining seo meta tags (#24)

--- app/head.tsx
+++ app/head.tsx
@@ -15,12 +15,30 @@ export default function Head({ title, subtitle }: { title: string; subtitle: str
     <>
       <title>EnvShare</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content={title} />
+      <meta name="description" content={subtitle} />
+      <meta name="theme-color" content="#000000" />
+      <meta name="title" content={title} />
+      <meta name="keywords" content="envshare, secure, secrets, share, environment, variables" />
+      <meta name="language" content="English" />
+      <meta name="revisit-after" content="7 days" />
+      <meta name="robots" content="all" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={baseUrl} />
       <meta property='og:image' content={url.toString()} />
       <meta property='og:title' content={title} />
-      <meta property='og:description' content={title} />
+      <meta property='og:description' content={subtitle} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={baseUrl} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={subtitle} />
+      <meta property="twitter:image" content={url.toString()} />
     </>
   );
 }
